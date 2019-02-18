51130f36afa23cc76731a8c4e4b7178d3651a617
JellyMeerkat <j.strenger@gmx.net>
2019-02-18 20:50:40 +0100

    everything works. some comments and code book missing

--- run_analysis.R
+++ run_analysis.R
@@ -18,8 +18,9 @@ download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUC
               "ProjectData.zip")
 
 # unzip Folder
-unzip("ProjectData.zip",exdir="ProjectData")
-
+if (!file.exists(zipFile)) {
+unzip("ProjectData.zip",exdir="ProjectData", mode = "wb")
+}
 ################################################################################
 # libraries:
 library(plyr)
@@ -49,17 +50,75 @@ train <- read.table("ProjectData/UCI HAR Dataset/train/X_train.txt",
 sub.train <- read.table("ProjectData/UCI HAR Dataset/train/subject_train.txt", 
                        header = FALSE, stringsAsFactors = FALSE)
 
+activity.train <- read.table("ProjectData/UCI HAR Dataset/train/y_train.txt", 
+                    header = FALSE, stringsAsFactors = FALSE)
+activity.test <- read.table("ProjectData/UCI HAR Dataset/test/y_test.txt", 
+                            header = FALSE, stringsAsFactors = FALSE)
+
+
+# set column names to features
+names(test) <- features
+names(train) <- features
+
+# set variable to differ between test and trainingset
+train$test_train <- "train"
+test$test_train <- "test"
+
+# add known or predicted outcome to the dataset
+test$activity <- activity.test$V1
+train$activity <- activity.train$V1
+
+# add subjects to the dataset
+test$subject <- sub.test$V1
+train$subject <- sub.train$V1
+
+# merge test and trainingset
+merged <- rbind(train,test)
+
 ################################################################################
 # Extracts only the measurements on the mean and standard deviation for 
 # each measurement.
 
+mean_std <- grep("mean\\(\\)|std", features)
+
+# add last two columns to selection vector (test_train, activity & subject)
+vec <- c(mean_std, 562:564)
+merged_mean_std <- merged[,vec]
+
 ################################################################################
 # Uses descriptive activity names to name the activities in the data set
 
+merged_mean_std$activity <- as.factor(merged_mean_std$activity)
+levels(merged_mean_std$activity) <- factor(activity_labels)
+
 ################################################################################
 # Appropriately labels the data set with descriptive variable names.
+
+names(merged_mean_std) <- 
+  gsub("^f", "frequencyDomain_", names(merged_mean_std)) %>%
+  gsub("^t", "timeDomain_", .) %>%
+  gsub("Gyro", "_gyroscope_", .) %>%
+  gsub("Acc", "_accelerometer_", .) %>%
+  gsub("Mag", "magnitude_", .)  %>%
+  gsub("\\-mean\\(\\)", "mean", .) %>%
+  gsub("\\-std\\(\\)", "standardDeviation", .) %>%
+  gsub("Jerk", "Jerk_", .) 
+
 
 ################################################################################
 # From the data set in step 4, creates a second, independent tidy data set 
 # with the average of each variable for each activity and each subject.
 
+## aggregate dataset as asked
+tidy <- aggregate(merged_mean_std[1:66], 
+                  c(list(merged_mean_std$subject), list(merged_mean_std$activity)),
+                  mean)
+
+# rename group variables
+names(tidy)[1:2] <- c("subject", "activity")
+
+
+
+################################################################################
+write.table(tidy, "tidy_data.txt", row.names = FALSE, 
+            quote = FALSE)
